d240523336f5979c0372a5c7485b9df3953e8aba
Roldan87 <jose.roldan1987@gmail.com>
2021-06-08 11:57:24 +0200

    queen_attack assignment

---
+++ python-queen-attack/queen_attack.py
@@ -0,0 +1,43 @@
+
+def board(wq, bq):
+
+    if wq[0] < 0 or wq[0] > 7:
+        raise ValueError("White Queen out of the board bounds")
+    elif wq[1] < 0 or wq[1] > 7:
+        raise ValueError("White Queen out of the board bounds")
+    elif bq[0] < 0 or bq[0] > 7:
+        raise ValueError("Black Queen out of the board bounds")
+    elif bq[1] < 0 or bq[1] > 7:
+        raise ValueError("Black Queen out of the board bounds")
+    elif wq[0] == bq[0] and wq[1] == bq[1]:
+        raise ValueError("Both Queens cannot share the same location")
+    else:
+        ans = [["_"] * 8 for num in range(8)]
+        ans[wq[0]][wq[1]] = "W"
+        ans[bq[0]][bq[1]] = "B"
+        ans = ["".join(row) for row in ans]
+        return ans
+    
+
+def can_attack(wq, bq):
+
+    if wq[0] < 0 or wq[0] > 7:
+        raise ValueError("White Queen out of the board bounds")
+    elif wq[1] < 0 or wq[1] > 7:
+        raise ValueError("White Queen out of the board bounds")
+    elif bq[0] < 0 or bq[0] > 7:
+        raise ValueError("Black Queen out of the board bounds")
+    elif bq[1] < 0 or bq[1] > 7:
+        raise ValueError("Black Queen out of the board bounds")
+    elif wq[0] == bq[0] and wq[1] == bq[1]:
+        raise ValueError("Both Queens cannot share the same location")
+    else:
+        if wq[0] == bq[0]:
+            return True
+        elif wq[1] == bq[1]:
+            return True
+        elif abs(wq[0] - bq[0]) == abs(wq[1] - bq[1]):
+            return True
+        else:
+            return False
+
