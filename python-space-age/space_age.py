bc2d8ba29ed0ce5925204bc77ceb4bbd0535e289
ibe-denaux <70886156+ibe-denaux@users.noreply.github.com>
2021-06-09 10:59:38 +0200

    Give feedback

--- python-space-age/space_age.py
+++ python-space-age/space_age.py
@@ -3,7 +3,9 @@ from typing import AsyncGenerator
 class SpaceAge:
 
     def __init__(self, user_age):
+        # COACHES' NOTE: why use both user_age and seconds? user age is redundant and opaque, since the unit of measurement is not clear from the name.
         self.user_age = user_age
+        # COACHES' NOTE: these are not sec(onds), these are years. a separate function to_years() would improve readability, that number looks hella ugly.
         self.earth_sec = self.user_age / 31557600
         self.seconds = user_age
 
@@ -12,6 +14,7 @@ class SpaceAge:
         return round(age_years, 2)
     
     def on_mercury(self):
+        # COACHES' NOTE: a magic number like this is best assigned to a variable first e.g. conversion_factor
         age_years = self.earth_sec / 0.2408467
         return round(age_years, 2)
 
@@ -39,3 +42,4 @@ class SpaceAge:
         age_years = self.earth_sec / 164.79132
         return round(age_years, 2)
 
+# COACHES' NOTE: Be careful of abbreviations, or even worse, wrong variable naming (seconds and years mean somthing very different).
